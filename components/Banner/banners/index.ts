e909f06dc25114b0dbb3662353f9ce62b5bfe0d0
Mathieu Marques <mathieumarques78@gmail.com>
2024-05-10 17:33:44 +0200

    wiki(chapters): add missing banners

--- components/Banner/banners/index.ts
+++ components/Banner/banners/index.ts
@@ -1,19 +1,41 @@
 import { type StaticImageData } from 'next/image';
 
+import { default as ANOINTED_OF_ASH } from './anointed-of-ash.webp';
+import { default as BRIBE_PRAETOR } from './bribe-praetor.webp';
+import { default as CONVERT_LEGION } from './convert-legion.webp';
+import { default as CONVERT_TRIBUTE } from './convert-tribute.webp';
+import { default as CORRUPT_TRIBUTE } from './corrupt-tribute.webp';
+import { default as DARK_AUGURY } from './dark-augury.webp';
 import { default as DEMAND_OF_SUPPLICATION } from './demand-of-supplication.webp';
 import { default as ELOCUTION } from './elocution.webp';
+import { default as EXPOSE_TREASON } from './expose-treason.webp';
+import { default as FORGED_ORDERS } from './forged-orders.webp';
 import { default as INFERNAL_AFFLICTION } from './infernal-affliction.webp';
 import { default as INFERNAL_JUGGERNAUT } from './infernal-juggernaut.webp';
 import { default as INFERNAL_NEGOCIATION } from './infernal-negociation.webp';
+import { default as PILFER_ARTIFACT } from './pilfer-artifact.webp';
+import { default as TARTAREAN_PATHFINDING } from './tartarean-pathfinding.webp';
+import { default as UNDYING_VIGOR } from './undying-vigor.webp';
 import { default as VANITY_S_ANNOINTED } from './vanity-s-annointed.webp';
 import { default as WITNESS_THE_MARTYR } from './witness-the-martyr.webp';
 
 export const BANNERS: Record<string, StaticImageData> = {
+  ANOINTED_OF_ASH,
+  BRIBE_PRAETOR,
+  CONVERT_LEGION,
+  CONVERT_TRIBUTE,
+  CORRUPT_TRIBUTE,
+  DARK_AUGURY,
   DEMAND_OF_SUPPLICATION,
   ELOCUTION,
+  EXPOSE_TREASON,
+  FORGED_ORDERS,
   INFERNAL_AFFLICTION,
   INFERNAL_JUGGERNAUT,
   INFERNAL_NEGOCIATION,
+  PILFER_ARTIFACT,
+  TARTAREAN_PATHFINDING,
+  UNDYING_VIGOR,
   VANITY_S_ANNOINTED,
   WITNESS_THE_MARTYR,
 };
