747ba81fddc5ec9a9cb8aa5cdf878ac0c00247dc
Mathieu Marques <mathieumarques78@gmail.com>
2024-05-19 18:24:07 +0200

    chore: use makePaths from Kiwi

--- app/constants.ts
+++ app/constants.ts
@@ -1,26 +1,10 @@
 import { join } from 'node:path';
-import { makeMenu, makeNextRoutes, walk } from '@korumite/kiwi/server';
-
-const trim = (text: string) => text.replace(/^\d+-/, '');
-
-/**
- * List all path crumbs from the provided tree of files.
- * The output is a map of named route to crumb arrays for further reference.
- * More notably, crumbs correspond to the real file name including digit
- * prefixes and file extensions.
- */
-const getPaths = (paths: string[][]) =>
-  paths.reduce<Record<string, Record<string, string[]>>>(
-    (accumulator, [realChapter, realSlug]) => {
-      if (realChapter && realSlug) {
-        const [chapter, slug] = [trim(realChapter), trim(realSlug)];
-        const path = (accumulator[chapter] ??= {});
-        path[slug] = [realChapter, `${realSlug}.md`];
-      }
-      return accumulator;
-    },
-    {},
-  );
+import {
+  makeMenu,
+  makeNextRoutes,
+  makePaths,
+  walk,
+} from '@korumite/kiwi/server';
 
 /** @deprecated Use `BASE_URLS` instead. */
 const BASE_URL = join(process.cwd(), 'markdown');
@@ -30,10 +14,10 @@ export const BASE_URLS = {
   CHAPTERS: join(BASE_URL, 'chapters'),
 };
 
-const TREE = walk(BASE_URLS.CHAPTERS);
+const TREE = walk(BASE_URLS.CHAPTERS) as [chapter: string, slug: string][];
 
 export const CHAPTERS = {
-  menu: makeMenu(TREE as [chapter: string, slug: string][]),
-  paths: getPaths(TREE),
+  menu: makeMenu(TREE),
+  paths: makePaths(TREE),
   routes: makeNextRoutes(TREE, ['chapter', 'slug']),
 } as const;
